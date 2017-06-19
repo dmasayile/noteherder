ac8d5ea257bca6390ebdd9e74826b6f16850c632
Your Full Name <dmasayile@gmail.com>
2017-06-19 11:24:55 -0400

    Adds complete Noteherder project w/ out homework

---
+++ SignIn.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+import './SignIn.css'
+import { auth, githubProvider } from './base'
+
+const SignIn = ({ authHandler }) => {
+  const authenticate = () => {
+    auth
+      .signInWithPopup(githubProvider)
+      .then((data) => {
+        authHandler(data.user)
+      })
+  }
+
+  return (
+    <button
+      className="SignIn"
+      onClick={authenticate}
+    >
+      Sign In With GitHub
+    </button>
+  )
+}
+
+export default SignIn
---
+++ src/App.js
@@ -0,0 +1,71 @@
+import React, { Component } from 'react'
+
+import './App.css'
+import Main from './Main'
+import SignIn from './SignIn'
+import SignOut from './SignOut'
+import base, { auth } from './base'
+
+class App extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      notes: {},
+      uid: null,
+    }
+  }
+
+  componentWillMount() {
+    base.syncState(
+      'notes',
+      {
+        context: this,
+        state: 'notes',
+      }
+    )
+  }
+
+  saveNote = (note) => {
+    if (!note.id) {
+      note.id = `note-${Date.now()}`
+    }
+    const notes = {...this.state.notes}
+    notes[note.id] = note
+    this.setState({ notes })
+  }
+
+  signedIn = () => {
+    return this.state.uid
+  }
+
+  authHandler = (userData) => {
+    this.setState({ uid: userData.uid })
+  }
+
+  signOut = () => {
+    auth
+      .signOut()
+      .then(() => this.setState({ uid: null }))
+    
+  }
+
+  renderMain = () => {
+    return (
+      <div>
+        <SignOut signOut={this.signOut} />
+        <Main notes={this.state.notes} saveNote={this.saveNote} />
+      </div>
+    )
+  }
+
+  render() {
+    return (
+      <div className="App">
+        { this.signedIn() ? this.renderMain() : <SignIn authHandler={this.authHandler} /> }
+      </div>
+    );
+  }
+}
+
+export default App;
